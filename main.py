b61edae7c5968f41e52b34f9b452911b35a0af86
cX01Tap <phanuruj.sura@bumail.net>
2025-01-29 01:15:35 +0700

    fix: update line7

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! its me phanuruj'
+    return 'Hello, BU:1650703554'
 
 if __name__ == 'phanuruj':
     app.run(debug=True, port=80)
